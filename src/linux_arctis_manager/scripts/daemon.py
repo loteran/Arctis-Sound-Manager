d477db3167b28254666851418ae6f9b1bdbf9724
Giacomo 'Mr. Wolf' Furlan <git@giacomofurlan.name>
2026-03-03 12:16:38 +0100

    Connect DbusAwake to the daemon

--- src/linux_arctis_manager/scripts/daemon.py
+++ src/linux_arctis_manager/scripts/daemon.py
@@ -5,6 +5,7 @@ from types import FrameType
 
 from linux_arctis_manager.core import CoreEngine
 from linux_arctis_manager.dbus_service import DbusManager
+from linux_arctis_manager.scripts.dbus_awake import DbusAwake
 from linux_arctis_manager.utils import project_version
 
 
@@ -22,9 +23,12 @@ async def main_async():
     await dbus_manager.start(core_engine)
 
     core_loop = asyncio.create_task(core_engine.start())
+    dbus_manager_loop = asyncio.create_task(DbusAwake.get_instance().start(core_engine))
 
     await dbus_manager.wait_for_stop()
     await core_loop
+
+    dbus_manager_loop.cancel()
 
 def sigterm_handler(
         sig: int,
