b4525734ac89bca5523e6c9074f6f7187c5c91ca
loteran <axel.valadon@gmail.com>
2026-06-08 15:10:38 +0200

    chore: add GPL-3.0 license header to tests/test_status_parser_fn.py

--- tests/test_status_parser_fn.py
+++ tests/test_status_parser_fn.py
@@ -1,3 +1,6 @@
+# Copyright (C) 2026 loteran
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 from arctis_sound_manager.status_parser_fn import int_int_mapping, int_str_mapping, on_off, percentage
 
 
